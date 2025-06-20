b2f3e94b0e4cea5dead918f3efe7c9c3028cb004
SachinPrasanth777 <142246653+SachinPrasanth777@users.noreply.github.com>
2025-06-20 15:07:11 +0530

    fix: server crash

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import testRouter from "./test/test.router";
+import usersController from "./users/users.controller";
 
 export default (): Router => {
   const app = Router();
   app.use("/test", testRouter());
+  app.use("/users", usersController());
   return app;
 };
--- server/src/middlewares/jwt.ts
+++ server/src/middlewares/jwt.ts
@@ -3,13 +3,10 @@ import * as JWT from "jsonwebtoken";
 import { JWT_EXPIRY } from "../shared/constants";
 
 export default function generateToken(data: string): string {
-  return JWT.sign({ data }, config.JWT_SECRET, JWT_EXPIRY);
+  return JWT.sign({ data }, config.JWT_SECRET, JWT_EXPIRY as JWT.SignOptions);
 }
 
 export function verifyToken(token: string) {
-  const data = JWT.verify(token, config.JWT_SECRET) as string;
-
-  return data as unknown as {
-    data: string;
-  };
+  const data = JWT.verify(token, config.JWT_SECRET) as { data: string };
+  return data;
 }
