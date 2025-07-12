be62ed0b140198996112cd981eae802b1e53bd7f
SachinPrasanth777 <142246653+SachinPrasanth777@users.noreply.github.com>
2025-07-12 15:16:55 +0530

    feat: deploy

--- src/pages/Upload.tsx
+++ src/pages/Upload.tsx
@@ -45,7 +45,7 @@ const UploadPage = () => {
       const formData = new FormData();
       formData.append('file', file);
 
-      const response = await fetch('http://localhost:8000/upload/', {
+      const response = await fetch('https://deployment-i6o2.onrender.com/upload/', {
         method: 'POST',
         body: formData,
       });
